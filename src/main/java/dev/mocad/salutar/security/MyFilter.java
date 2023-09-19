4af9dc82517dab5525d59637f88d10cc1577781c
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-09-19 19:07:04 -0300

    commit 3

--- src/main/java/dev/mocad/salutar/security/MyFilter.java
+++ src/main/java/dev/mocad/salutar/security/MyFilter.java
@@ -1,18 +1,2 @@
-package dev.mocad.salutar.security;
-
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
-public class MyFilter extends OncePerRequestFilter {
-
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-    filterChain.doFilter(request, response);
-  }
+package dev.mocad.salutar.security;public class MyFilter {
 }
